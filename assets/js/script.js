4d4bc73be79d047e636b1e1aebcf914e02282fd3
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-03-06 10:57:48 +0400

    Header scroll state js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,9 @@ const $navToggler = document.querySelector("[data-nav-toggler]");
 
 $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
 
+// Header scroll state
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
